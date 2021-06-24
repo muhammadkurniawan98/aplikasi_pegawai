1bd3895c6583903acf0d5ce7eec6d539ce575713
muhammadkurniawan98 <muhammadadekurniawan98@gmail.com>
2021-06-24 16:15:54 +0700

    Remove aplikasi_pegawai

--- resources/views/user/layouts/includes/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; SMA Negeri 1 Seputih Banyak {{ date('Y') }}</span>
-        </div>
-    </div>
-</footer>
