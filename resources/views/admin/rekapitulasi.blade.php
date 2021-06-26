f1372bdb8bb3c10e6f66d6d1a45b9dfa72d929a0
muhammadkurniawan98 <muhammadadekurniawan98@gmail.com>
2021-06-26 10:27:26 +0700

    update aplikasi_pegawai version 1.0.0

--- resources/views/admin/rekapitulasi.blade.php
+++ resources/views/admin/rekapitulasi.blade.php
@@ -54,7 +54,7 @@
                                 <div class="h5 mb-0 font-weight-bold text-gray-800">{{ pegawaiPensiun() }} - Orang</div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-user-check fa-2x text-gray-300"></i>
+                                <i class="fas fa-user-slash fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
