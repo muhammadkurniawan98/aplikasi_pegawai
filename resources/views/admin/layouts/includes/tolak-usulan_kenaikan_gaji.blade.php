c2e74089485ecd6447cfca840e51b2d4363ff04a
muhammadkurniawan98 <muhammadadekurniawan98@gmail.com>
2021-07-23 01:08:57 +0700

    update-12

---
+++ resources/views/admin/layouts/includes/tolak-usulan_kenaikan_gaji.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="tolak_usulan_kenaikan_gaji" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            Pesan
+        </div>
+        <div class="modal-body">
+            Anda yakin ingin menolak usulan kenaikan gaji ini?
+        </div>
+
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+            <a id="submit" class="btn btn-danger">Ya</a>
+        </div>
+    </div>
+</div>
+</div>
---
+++ resources/views/admin/layouts/includes/tolak-usulan_kenaikan_pangkat.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="tolak_usulan_kenaikan_pangkat" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                Pesan
+            </div>
+            <div class="modal-body">
+                Anda yakin ingin menolak usulan kenaikan pangkat ini?
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                <a id="submit" class="btn btn-danger">Ya</a>
+            </div>
+        </div>
+    </div>
+</div>
